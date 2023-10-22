bcbf667877d64a899ed5052b4b4decb03d2ae222
danilp10 <daniel8talavera@gmail.com>
2023-10-22 15:45:38 +0100

    code optimized

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ import search
 ab = search.GPSProblem('A', 'B'
                        , search.romania)
 
-#print(search.breadth_first_graph_search(ab).path())
-#print(search.depth_first_graph_search(ab).path())
+print(search.breadth_first_graph_search(ab).path())
+print(search.depth_first_graph_search(ab).path())
 
 print(search.branch_and_bound_search(ab).path())
 # Result:
